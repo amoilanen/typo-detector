ec2544413906b6a9cb7cdf99f599802e931928a3
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-07-30 23:03:41 +0300

    Add non-implicits based API

--- src/main/scala/fi/antivanov/typo/detector/TypoDetector.scala
+++ src/main/scala/fi/antivanov/typo/detector/TypoDetector.scala
@@ -4,19 +4,25 @@ import LevensteinDistance.computeDistance
 
 object TypoDetector {
 
-  object StringWithTypoDetection {
+  class StringWithTypoDetection(str: String) {
 
-    class StringWithTypoDetection(str: String) {
-
-      def isTypoOf(otherStr: String, maxMistypedSymbols: Int = 2): Boolean = {
-        val distance = computeDistance(str, otherStr)
-        distance > 0 && distance <= maxMistypedSymbols
-      }
-
-      def editDistanceFrom(otherStr: String) =
-        computeDistance(str, otherStr)
+    def isTypoOf(otherStr: String, maxMistypedSymbols: Int = 2): Boolean = {
+      val distance = computeDistance(str, otherStr)
+      distance > 0 && distance <= maxMistypedSymbols
     }
+
+    def editDistanceFrom(otherStr: String): Int =
+      computeDistance(str, otherStr)
+  }
+
+  object StringWithTypoDetection {
 
     implicit def stringToString(s: String) = new StringWithTypoDetection(s)
   }
+
+  def isTypoOf(str: String, otherStr: String): Boolean =
+    new StringWithTypoDetection(str).isTypoOf(otherStr)
+
+  def editDistanceFrom(str: String, otherStr: String): Int =
+    new StringWithTypoDetection(str).editDistanceFrom(otherStr)
 }

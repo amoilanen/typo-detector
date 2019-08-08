403cc0cb8fe7077b9da89d5f55543d0ce323fbe7
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-08-08 23:23:43 +0300

    Implement method for searching a string or its typo in text

--- src/main/scala/io/github/antivanov/typo/detector/TypoDetector.scala
+++ src/main/scala/io/github/antivanov/typo/detector/TypoDetector.scala
@@ -6,11 +6,35 @@ object TypoDetector {
 
   val DefaultMaxMistypedSymbols = 2
 
+  private def stripPunctuation(str: String): String =
+    str.replaceAll("[\\Q,;:.-'\\E]", "")
+
+  private def getWords(str: String): Array[String] =
+    stripPunctuation(str).split("\\s+")
+
   class StringWithTypoDetection(str: String) {
 
     def isTypoOf(otherStr: String, maxMistypedSymbols: Int = DefaultMaxMistypedSymbols): Boolean = {
       val distance = computeDistance(str, otherStr)
       distance > 0 && distance <= maxMistypedSymbols
+    }
+
+    def equalsOrTypoOf(otherStr: String, maxMistypedSymbols: Int = DefaultMaxMistypedSymbols): Boolean =
+      str.equals(otherStr) || isTypoOf(otherStr)
+
+    def containsExactOrTypoOf(strToFind: String, maxMistypedSymbols: Int = DefaultMaxMistypedSymbols): Boolean = {
+      val strWords = getWords(str)
+      val strToFindWords = getWords(strToFind)
+
+      val possiblePositions = (0 to (strWords.length - strToFindWords.length))
+
+      val positionOfStrToFind = possiblePositions.find { position: Int =>
+        val strWordsAtPosition = strWords.slice(position, position + strToFindWords.length)
+        strWordsAtPosition.zip(strToFindWords).forall { case (strWord, strToFindWord) =>
+          new StringWithTypoDetection(strWord).equalsOrTypoOf(strToFindWord, maxMistypedSymbols)
+        }
+      }
+      positionOfStrToFind.isDefined
     }
 
     def editDistanceFrom(otherStr: String): Int =
@@ -24,6 +48,12 @@ object TypoDetector {
 
   def isTypoOf(str: String, otherStr: String, maxMistypedSymbols: Int = DefaultMaxMistypedSymbols): Boolean =
     new StringWithTypoDetection(str).isTypoOf(otherStr, maxMistypedSymbols)
+
+  def equalsOrTypoOf(str: String, otherStr: String, maxMistypedSymbols: Int = DefaultMaxMistypedSymbols): Boolean =
+    new StringWithTypoDetection(str).equalsOrTypoOf(otherStr, maxMistypedSymbols)
+
+  def containsExactOrTypoOf(str: String, otherStr: String, maxMistypedSymbols: Int = DefaultMaxMistypedSymbols): Boolean =
+    new StringWithTypoDetection(str).containsExactOrTypoOf(str, maxMistypedSymbols)
 
   def editDistanceFrom(str: String, otherStr: String): Int =
     new StringWithTypoDetection(str).editDistanceFrom(otherStr)
--- src/test/scala/io/github/antivanov/typo/detector/TypoDetectorSpec.scala
+++ src/test/scala/io/github/antivanov/typo/detector/TypoDetectorSpec.scala
@@ -15,23 +15,82 @@ class TypoDetectorSpec extends FreeSpec with Matchers {
     }
 
     "differs by two symbols => typo" in {
-      "Helsinki".isTypoOf("Helsenke") shouldBe true
+      "Achilles".isTypoOf("Achilez") shouldBe true
     }
 
     "differs by one symbol => typo" in {
-      "Helsinki".isTypoOf("Helsenki") shouldBe true
+      "Achilles".isTypoOf("Achiles") shouldBe true
     }
 
     "identical => not a typo" in {
-      "Helsinki".isTypoOf("Helsinki") shouldBe false
+      "Achilles".isTypoOf("Achilles") shouldBe false
     }
 
     "differs by three symbols => not a typo, too many different symbols" in {
-      "Helsinki".isTypoOf("Helsingfors") shouldBe false
+      "Achilles".isTypoOf("Atrides") shouldBe false
     }
 
     "allows to specify max number of different symbols" in {
-      "Helsinki".isTypoOf("Helsingfors", maxMistypedSymbols = 5) shouldBe true
+      "Achiles".isTypoOf("Achilles", maxMistypedSymbols = 5) shouldBe true
+    }
+  }
+
+  "equalsOrTypo" - {
+
+    "recognize a typo" in {
+      "Achilles".equalsOrTypoOf("Achillez") shouldBe true
+    }
+
+    "recognize exact same word" in {
+      "Achilles".equalsOrTypoOf("Achilles") shouldBe true
+    }
+
+    "recognize different words, not a typo" in {
+      "Achilles".equalsOrTypoOf("Hector") shouldBe false
+    }
+  }
+
+  "containsExactOrTypoOf" - {
+
+    /*
+     * "The Iliad" Book I "The contention of Achilles and Agamemnon"
+     * http://www.gutenberg.org/cache/epub/6130/pg6130.txt
+     */
+    val text =
+      """
+        |  Achilles' wrath, to Greece the direful spring
+        |  Of woes unnumbered, heavenly goddess, sing!
+        |  That wrath which hurled to Pluto's gloomy reign
+        |  The souls of mighty chiefs untimely slain;
+        |  Whose limbs unburied on the naked shore,
+        |  Devouring dogs and hungry vultures tore.
+        |  Since great Achilles and Atrides strove,
+        |  Such was the sovereign doom, and such the will of Jove!
+        |""".stripMargin
+
+    "recognize exact string" in {
+      "wrath".containsExactOrTypoOf("wrath") shouldBe true
+    }
+
+    "recognize exact contained string" in {
+      text.containsExactOrTypoOf("wrath") shouldBe true
+    }
+
+    "recognize typo" in {
+      "greet".containsExactOrTypoOf("great") shouldBe true
+    }
+
+    "recognize contained typo" in {
+      text.replaceFirst("great", "greet").containsExactOrTypoOf("great") shouldBe true
+    }
+
+    "recognize typo if string consists of several words" in {
+      text.replaceFirst("great", "greet")
+        .replaceAll("Achilles", "Achilez").containsExactOrTypoOf("great Achilles") shouldBe true
+    }
+
+    "return false if not contained" in {
+      text.containsExactOrTypoOf("Priam") shouldBe false
     }
   }
 }
